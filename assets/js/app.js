a6d992c93eac0e2fdb2cccce1988aee4d79d0ed1
Diana <dianachcastro@yahoo.es>
2017-12-09 18:33:50 -0300

    textarea sin freno con jquery

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	/*contador 140 caract. con keydown*/
+	init_contadorTa("taComentario","contadorTaComentario", 140);
+
+function init_contadorTa(idtextarea, idcontador,max)
+{
+    $("#"+idtextarea).keydown(function()
+            {
+                updateContadorTa(idtextarea, idcontador,max);
+            });
+    
+    $("#"+idtextarea).change(function()
+    {
+            updateContadorTa(idtextarea, idcontador,max);
+    });
+    
+}
+
+function updateContadorTa(idtextarea, idcontador,max)
+{
+    var contador = $("#"+idcontador);
+    var ta =     $("#"+idtextarea);
+    contador.html("0/"+max);
+    
+    contador.html(ta.val().length+"/"+max);
+    if(parseInt(ta.val().length)>max)
+    {
+        ta.val(ta.val().substring(0,max-1));
+        contador.html(max+"/"+max);
+    }
+init_contadorTa("taComentario","contadorTaComentario", 140);
+}
+});
